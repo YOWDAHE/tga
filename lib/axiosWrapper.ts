3447b51a5abf3296248a6dbca8e59a0768ab7157
yowdahe <ketemayodahe@gmail.com>
2025-08-09 09:57:23 +0300

    axios wrapper correction

--- lib/axiosWrapper.ts
+++ lib/axiosWrapper.ts
@@ -4,8 +4,8 @@ import { getRequestCookies } from "./axiosContext";
 
 const api: AxiosInstance = axios.create({
   // baseURL: process.env.BACKEND_API_URL,
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  // baseURL: 'http://localhost/office/api',
+  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'http://frontend:3001/office/api',
   withCredentials: true,
 });
 
