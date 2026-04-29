f3aa6df9f0a4873654b173cc2b34adb168472ac2
yowdahe <ketemayodahe@gmail.com>
2026-04-29 21:35:17 +0300

    contact page url update

--- app/actions/homepage.actions.ts
+++ app/actions/homepage.actions.ts
@@ -2,7 +2,7 @@ import { get, post, put } from '@/lib/axiosWrapper';
 
 export async function updateHomepage(updateData: any) {
     try {
-        const res = await put('/homepage', updateData);
+        const res = await put('/landing', updateData);
         if (!res.data.data) throw new Error(res.data.error || 'Failed to update homepage');
         return { success: true, data: res.data.data };
     } catch (error: any) {
